d34a688d6cc4aeb321518f3e0ed3e928be3895c3
DarkbornEmperor <darkandrai1987@gmail.com>
2021-07-29 09:50:20 -0400

    Twitcher changes

--- lua/entities/npc_vj_cofraom_twitcherhd/init.lua
+++ lua/entities/npc_vj_cofraom_twitcherhd/init.lua
@@ -7,14 +7,6 @@ include('shared.lua')
 -----------------------------------------------*/
 ENT.CanFlinch = 0
 ENT.AnimTbl_Death = {ACT_DIESIMPLE}
----------------------------------------------------------------------------------------------------------------------------------------------
-function ENT:Twitcher_CustomOnPreInitialize()
-if self:GetModel() == "models/vj_cofr/aom/zombiehd2.mdl" then
-    self:SetBodygroup(0,math.random(0,9))
-else
-    self:SetBodygroup(0,math.random(0,10))
-end	
-end
 /*-----------------------------------------------
 	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
